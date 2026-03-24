34aa09d3de7b0db723213d0f8f12f9136b6e93ce
hperezmeseguer <hperezmeseguer@danielcastelao.org>
2026-03-24 13:57:14 +0100

    código de la clase casilla comentado

--- casilla.py
+++ casilla.py
@@ -1,16 +1,19 @@
 class Casilla:
     def __init__(self):
+        # Guarda si en la casilla hay una nave o si ya fue atacada la casilla
+        #Por defecto ponemos que no
         self.nave = None
         self.visitada = False
 
+    # Metodo que comprueba el estado de la casilla
     def disparar(self):
-        if self.visitada:
+        if self.visitada: # Si ya disparamos a la casilla printea eso
             print("Ya disparaste aquí")
             return None
 
         self.visitada = True
 
-        if self.nave is None:
+        if self.nave is None: # Si disparamos y no hay ninguna nave printea eso
             print("Agua")
             return 0
 
