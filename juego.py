85fac20f749cf13d6da5e5fcff4975380520b856
hperezmeseguer <hperezmeseguer@danielcastelao.org>
2026-03-24 13:50:52 +0100

    código de la clase juego comentado

--- juego.py
+++ juego.py
@@ -3,25 +3,30 @@ from tablero import Tablero
 class Juego:
     def __init__(self):
         self.tablero = Tablero()
-        self.lanzar_ataque(1, 1)
-        self.lanzar_ataque(1, 2)
-        self.lanzar_ataque(1, 3)
-        self.lanzar_ataque(1, 4)
-        self.lanzar_ataque(1, 4)
-        self.lanzar_ataque(1,5)
-        self.lanzar_ataque(1, 6)
+        self.lanzar_ataque(1, 1) # Lanzamos los ataques a diferentes ubicaciones (x, y)
+        self.lanzar_ataque(1, 2) # Lanzamos los ataques a diferentes ubicaciones (x, y)
+        self.lanzar_ataque(1, 3) # Lanzamos los ataques a diferentes ubicaciones (x, y)
+        self.lanzar_ataque(1, 4) # Lanzamos los ataques a diferentes ubicaciones (x, y)
+        self.lanzar_ataque(1, 4) # Lanzamos los ataques a diferentes ubicaciones (x, y)
+        self.lanzar_ataque(1,5) # Lanzamos los ataques a diferentes ubicaciones (x, y)
+        self.lanzar_ataque(1, 6) # Lanzamos los ataques a diferentes ubicaciones (x, y)
 
 
     def inicializar_naves(self):
         pass
 
+    # Creación de un metodo que muestre el resultado de los ataques
+
     def mostrar_resultado(self, resultado):
-        if resultado == 0:
+        if resultado == 0: # No se tocó ninguna nave
             print("Agua")
-        elif resultado == 1:
+        elif resultado == 1: # Se tocó parte de una nave
             print("Tocado")
-        elif resultado == 2:
+        elif resultado == 2: # Se tocaron todas las partes de la nave y se hundió
             print("Hundido")
+
+
+    # Creación de un metodo que muestre el resultado de los ataques
 
     def lanzar_ataque(self, x, y):
         print(f"Atacando {x}, {y}")
