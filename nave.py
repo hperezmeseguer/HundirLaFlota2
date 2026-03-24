e9720554f2116bda0e7f6d544c00719c5ecedcbb
hperezmeseguer <hperezmeseguer@danielcastelao.org>
2026-03-24 13:58:40 +0100

    código de la clase nave comentado

--- nave.py
+++ nave.py
@@ -4,10 +4,10 @@ class Nave:
         self.tipo = tipo
         self.vida = vida
 
-
+    # Metodo que recibe el disparo y gestiona las vidas de las naves
     def recibir_disparo(self):
-        self.vida -= 1
+        self.vida -= 1 # Si una nave fue tocada le resta una vida
         print(f"[LOG] Vidas restantes de {self.nombre}: {self.vida}")
 
-        if self.vida == 0:
+        if self.vida == 0: # Si le quedan cero vidas printea que se hundió esa nave
             print(f"[LOG] Nave hundida")
